8687bd4b612827dde1380fed3116141e505e5f6c
AAA114514AAA1919810 <bourdonbenoit12@gmail.com>
2026-01-20 04:50:49 +0800

    Add files via upload

--- TP2.cxx
+++ TP2.cxx
@@ -1,107 +1,272 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
-//  EXERCICE 1
-// struct Point{
-//     float abs, ord;
-// };
+// EXERCICE 1
+struct Point{
+    float abs, ord;
+};
 
-// Point saisirPoint(){
-//     Point Pa;
-//     cout << "absci :";
-//     cin >> Pa.abs;
-//     cout << "ord : ";
-//     cin >> Pa.ord;
-//     return Pa;
-// }
+Point saisirPoint(){
+    Point Pa;
+    cout << "absci :";
+    cin >> Pa.abs;
+    cout << "ord : ";
+    cin >> Pa.ord;
+   return Pa;
+}
 
-// void afficherPoint(Point p){
-//     cout << p.abs << endl;
-//     cout << p.ord << endl;
-// }
+void afficherPoint(Point p){
+    cout << p.abs << endl;
+    cout << p.ord << endl; 
+}
 
-// float calculerDistance(Point p1, Point p2){
-//     float res;
-//     res = sqrt((p2.abs-p1.abs)**2+(p2.ord-p1.ord)**2);
-//     return res;
-// }
+float calculerDistance(Point p1, Point p2){
+    float res;
+    res = sqrt((p2.abs - p1.abs) * (p2.abs - p1.abs) + (p2.ord - p1.ord) * (p2.ord - p1.ord));
+    return res;
+}
 
-// bool estTriangle(Point p1, Point p2, Point p3){
-//     double a = calculerDistance(p1, p2);
-//     double b = calculerDistance(p2, p3);
-//     double c = calculerDistance(p3, p1);
-//     if(a< b+c){
-//         if(b<a+c){
-//             if(c < a+b){
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// }
-
-// int main(){
-//     Point A;
-//     Point B;
-//     Point C;
-//     A = saisirPoint();
-//     B = saisirPoint();
-//     C = saisirPoint();
-//     cout << A << endl;
-//     cout<< calculerDistance(A,B) <<endl;
-//     if(estTriangle(A,B,C)==true){
-//         cout<< "c'est un triangle"<<endl;
-//     }
-//     else{
-//         cout<<"ce n'est pas triangle"<<endl;
-//     }
-//     return 0;
-// }
-
+bool estTriangle(Point p1, Point p2, Point p3){ 
+    double a = calculerDistance(p1, p2);
+    double b = calculerDistance(p2, p3);
+    double c = calculerDistance(p3, p1);
+    if(a< b+c){
+        if(b<a+c){
+            if(c < a+b){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+/*
+int main(){
+    Point A;
+    Point B;
+    Point C;
+    A = saisirPoint();
+    B = saisirPoint();
+    C = saisirPoint();
+    afficherPoint(A);
+    cout << calculerDistance(A,B) <<endl;
+    if(estTriangle(A,B,C)==true){
+        cout << "c'est un triangle"<<endl; 
+    }
+    else{
+        cout <<"ce n'est pas triangle"<<endl;
+    }
+    return 0;
+ }
+*/
 // EXERCICE2
 struct Eleve{
     string nom, prenom;
-    int an;
-    char se;
+    int annee;
+    char sex;
     float moy;
-}
+};
+
 struct Class{
-    int ne;
-    Eleve class[25]
-}
-int Nbel;
-Eleve class[Nbel];
+    int nbe;
+    Eleve eleves[25];
+};
+
 Eleve saisirEleve(){
     Eleve e;
-    cin>>e.nom;
-    cin>>e.prenom;
-    cin>>e.an;
-    cin>>e.se;
-    while(e.se=! "M" or "F"){
-        cin>>e.se;
+    cout << "Nom : ";
+    cin >> e.nom;
+
+    cout << "Prenom : ";
+    cin >> e.prenom;
+
+    cout << "Age : ";
+    cin >> e.annee;
+
+    cout << "Sexe (M/F) : ";
+    cin >> e.sex;
+    while(e.sex != 'M' && e.sex != 'F'){
+        cin>>e.sex;
     }
     cin>>e.moy;
     return e; 
 }
 
-int calculerAge(int e.an){
-    int age = getCurrentYear() - e.an;
+int getCurrentYear() {
+    // pour cette partie j'ai du chercher sur internet, je ne sais pas comment obtenir l'annee actuelle
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    return now->tm_year + 1900; // tm_year = annees depuis 1900
+}
+
+int calculerAge(int annee){
+    int age = getCurrentYear() - annee;
     return age;
 }
 void afficherEleve(Eleve e){
-    cout<<e <<endl;
-    cout<<calculerAge(e.an)<<endl;
+    cout << "Nom : " << e.nom << endl;
+    cout << "Prenom : " << e.prenom << endl;
+    cout << "Annee de naissance : " << e.annee << endl;
+    cout << "Sexe : " << e.sex << endl;
+    cout << "Moyenne : " << e.moy << endl;
+    cout << "Age : " << calculerAge(e.annee) << " ans" << endl;
 }
 
-Class saisirClass(int n){
+Class saisirClasse(int n) {
+    Class c;
+    // au cas où la valeur rentree n'est pas dans l'intervalle autorisee d'eleves
+    if (n < 1) n = 1;
+    if (n > 25) n = 25;
     
+    c.nbe = n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "info Eleve " << i + 1 << endl;
+        c.eleves[i] = saisirEleve();
+    }
+
+    return c;
 }
+
+void afficherClasse(Class c) {
+    cout << "___ Classe (nombre d'eleves : " << c.nbe << ")___" << endl;
+    for (int i = 0; i < c.nbe; i++) {
+        cout << "info Eleve " << i + 1 << endl;
+        cout << "Nom : " << c.eleves[i].nom << endl;
+        cout << "Prenom : " << c.eleves[i].prenom << endl;
+        cout << "Annee de naissance : " << c.eleves[i].annee << endl;
+        cout << "Sexe : " << c.eleves[i].sex << endl;
+        cout << "Moyenne : " << c.eleves[i].moy << endl;
+        cout << "Age : " << calculerAge(c.eleves[i].annee) << " ans" << endl;
+    }
+}
+
+
+bool existeEleve(Class c, string nom, string prenom) {
+    for (int i = 0; i < c.nbe; i++) {
+        if (c.eleves[i].nom == nom && c.eleves[i].prenom == prenom) {
+            return true; // dans la class
+        }
+    }
+    return false; // pas dans la class
+}
+
+int indiceEleve(Class c, string nom, string prenom) {
+    for (int i = 0; i < c.nbe; i++) {
+        if (c.eleves[i].nom == nom && c.eleves[i].prenom == prenom) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+Class ajouterEleve(Class c, Eleve e) {
+    if (c.nbe < 25) {          // verifie que class non complete
+        c.eleves[c.nbe] = e;   // ajouter l'eleve
+        c.nbe++;               // nb eleves + 1
+    } 
+    else {
+        cout << "Erreur : la classe est pleine " << endl;
+    }
+    return c;                  // renvoie classe modifiee
+}
+
+Class supprimerEleve(Class c, string nom, string prenom) {
+    int indice = -1;
+
+    // Chercher indice eleve
+    for (int i = 0; i < c.nbe; i++) {
+        if (c.eleves[i].nom == nom && c.eleves[i].prenom == prenom) {
+            indice = i;
+            break;
+        }
+    }
+
+    if (indice == -1) {
+        cout << "L'eleve " << nom << " " << prenom << " non trouve " << endl;
+        return c; // on ne change pas la classe si eleve non trouve
+    }
+
+    // remplace l'info de chaque eleve par l'eleve suivant
+    for (int i = indice; i < c.nbe - 1; i++) {
+        c.eleves[i] = c.eleves[i + 1];
+    }
+
+    c.nbe--; // nb eleve - 1
+    cout << "L'eleve " << nom << " " << prenom << " a ete supprime" << endl;
+
+    return c;
+}
+
+int compterNb(Class c, char s) {
+    int n= 0;
+    for (int i = 0; i < c.nbe; i++) {
+        if (c.eleves[i].sex == s) {
+            n++;
+        }
+    }
+    return n;
+}
+
+float calculerMoyenne(Class c) {
+
+    float somme = 0.0;
+    for (int i = 0; i < c.nbe; i++) {
+        somme += c.eleves[i].moy;
+    }
+
+    return somme / c.nbe;
+}
+
+
 
 int main(){
-    cin>> Nbel;
-    while(Nbel>25){
-        cin>> Nbel;
-}
+    int n;
+    cout << "nb eleves de la classe : " << endl;
+    cin >> n;
+
+    // Saisie eleves
+
+    // pour l'etape de création des eleves, lorsque je rentre le sexe, le programme freeze, je ne sais pas pourquoi,
+    // donc je n'ai pas pu tester le reste
+    Class maClasse = saisirClasse(n);
+
+    // Affichage eleves
+    cout << endl << "___ Affichage des eleves de la classe ___" << endl;
+    afficherClasse(maClasse);
+
+    // Saisie + ajout nouvel eleve s'il n'existe pas
+    cout << endl << "___ Ajouter un nouvel eleve ___" << endl;
+    Eleve nouveau = saisirEleve();
+    if (!existeEleve(maClasse, nouveau.nom, nouveau.prenom)) {
+        maClasse = ajouterEleve(maClasse, nouveau);
+        cout << "Nouvel eleve ajoute." << endl;
+    } else {
+        cout << "Cet eleve existe dejà dans la classe !" << endl;
+    }
+
+    // Saisie ou suppression eleve s'il existe
+    cout << endl << "___ Supprimer un eleve ___" << endl;
+    string nomSuppr, prenomSuppr;
+    cout << "Nom de l'eleve à supprimer : " << endl;
+    cin >> nomSuppr;
+    cout << "Prenom de l'eleve à supprimer : " << endl;
+    cin >> prenomSuppr;
+    maClasse = supprimerEleve(maClasse, nomSuppr, prenomSuppr);
+
+    // Affichage eleves apres modifications
+    cout << endl << "=== Classe apres modifications ===" << endl;
+    afficherClasse(maClasse);
+
+    // nb filles garçons
+    int nbFilles = compterNb(maClasse, 'F');
+    int nbGarcons = compterNb(maClasse, 'M');
+    cout << endl << "Nombre de filles : " << nbFilles << endl;
+    cout << "Nombre de garçons : " << nbGarcons << endl;
+
+    // Moy generale classe
+    cout << "Moyenne generale de la classe : " << calculerMoyenne(maClasse) << endl;
+
     return 0;
 }
--- hello.cxx
+++ hello.cxx
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-
+/* Exercice 1 */
 
 int additionner(int a, int b){
     return a + b;
@@ -19,34 +19,124 @@ void initTab(int T[50]){
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<> dist(a,b);
-int i = 0;
-for (int i ; i < 50 ; i++)
-{
-    int val;
-    val = dist(rng); //génère un nombre aléatoire entre a et b,
-                     //peut être répété plusieurs fois
-    T[i] = val;
-}
+    for (int i = 0; i < 50 ; i++)
+    {
+        int val;
+        val = dist(rng); //génère un nombre aléatoire entre a et b,
+                         //peut être répété plusieurs fois
+        T[i] = val;
+    }
 }
 
 
-int printTab(int T[50] , int nbCol ){
+void printTab(int T[50] , int nbCol ){
     int i;
     for (int i = 0; i < 50 ; i++ ){
-        
-        if (i%10 == 0){
+        /* && = and en python */
+        if (i%10 == 0 && i != 0){
             cout << T[i] << endl;
         }
         else{
             cout << T[i];
         }
     }
+cout << endl;
+}
 
+int addElementTab(int T[50])
+{
+    int somme = 0;
 
+    initTab(T);
+
+    for (int i = 0; i < 50; i++)
+    {
+        somme = additionner(somme, T[i]);
+    }
+
+    return somme;
 }
 
 
+/* Exercice 2 */
+
+int arrondiSup(double n)
+{       /* pourquoi parenthèse sur int sur c++ ??? aucun sense */
+    int arrondi = (int)n;  // on garde partie entière, (int) donne arrondi inférieur
+
+    if (n == arrondi){
+        return arrondi;
+    }
+    else{
+        return arrondi + 1;
+    }
+}
+
+int tempsDeReaction(bool fatigue, bool alcool)
+{
+    int temps = 1; 
+
+    if (fatigue)
+    {
+        temps = temps + 1;
+    }
+
+    if (alcool)
+    {
+        temps++;
+        temps = temps * 2;
+    }
+
+    return temps;
+}
+
+int kmh2ms(double vitesseKmh)
+{
+    double vitesseMs = vitesseKmh / 3.6;  //  m/s = km/h ÷ 3.6
+    return arrondiSup(vitesseMs);
+}
+
+int distanceReaction(double vitesseKmh, bool fatigue, bool alcool)
+{
+    int vitesseMs = kmh2ms(vitesseKmh);
+    int tempsReac = tempsDeReaction(fatigue, alcool);
+
+    double distance = vitesseMs * tempsReac;
+    return arrondiSup(distance);
+}
+
+int distanceFreinage(double vitesseKmh)
+{   // d = (v/10)**2, mais ** ne marche pas en c++, donc * soi-même
+    double distance = (vitesseKmh / 10.0) * (vitesseKmh / 10.0); 
+    return arrondiSup(distance);
+}
+
+int distanceTotaleSec(double vitesseKmh, bool fatigue, bool alcool)
+{
+    int distanceReac = distanceReaction(vitesseKmh, fatigue, alcool);
+    int distanceFrein = distanceFreinage(vitesseKmh);
+
+    int distanceTotale = distanceReac + distanceFrein;
+    return distanceTotale;
+}
+
+int distanceTotaleHum(double vitesseKmh, bool fatigue, bool alcool)
+{
+    int distanceReac = distanceReaction(vitesseKmh, fatigue, alcool);
+    int distanceFrein = distanceFreinage(vitesseKmh);
+
+    int distanceTotale = distanceReac + 2 * distanceFrein;
+    return distanceTotale;
+}
+
+int distanceEtage(int distance)
+{
+    double nbetages = distance / 2.5;
+    return arrondiSup(nbetages);
+}
+
 int main(){
+// Exercice 1
 /*  int a, b ,som;
     cout << "saisir 2 entiers :";
     cin >> a >> b;
@@ -54,10 +144,76 @@ int main(){
     som = additionner(a, b);
     cout << som << endl;
 */
+/* 
     int T[50];
     initTab(T);
 
     int nbCol = 10;
-    cout << printTab( T, nbCol);
+    printTab( T, nbCol);
+    return 0;
+*/
+/*
+    int T[50];
+    int somme = addElementTab(T);
+
+    printTab(T, 10);
+    cout << "Somme du tableau : " << somme << endl;
+
+    return 0;
+*/
+
+// Exercice 2
+    double vitesseKmh;
+    bool fatigue, alcool;
+
+    cout << "Estimation simplifiee de la distance d'arret d'un vehicule" << endl;
+    // \t sert à passer de colonne dans un tab (pour une fois c++ plus pratique que python)
+    cout << "km/h\t(a)\t(b)\t(1)\t(2)\t(3)\t(4)\t(5)" << endl;
+
+    //vitesses de 20 à 130 km/h par pas de 10
+    for (vitesseKmh = 20; vitesseKmh <= 130; vitesseKmh += 10)
+    {
+        // Ligne du tableau
+        cout << (int)vitesseKmh << "km/h\t";
+
+        // (a) distance de réaction normale : ni fatigue ni alcool
+        fatigue = false;
+        alcool = false;
+        cout << distanceReaction(vitesseKmh, fatigue, alcool) << "m\t";
+
+        // (b) distance freinage
+        cout << distanceFreinage(vitesseKmh) << "m\t";
+
+        // (1) distance normale
+        cout << distanceTotaleSec(vitesseKmh, false, false) << "m\t";
+
+        // (2) fatigue
+        cout << distanceTotaleSec(vitesseKmh, true, false) << "m\t";
+
+        // (3) alcool
+        cout << distanceTotaleSec(vitesseKmh, false, true) << "m\t";
+
+        // (4) fatigue + alcool
+        cout << distanceTotaleSec(vitesseKmh, true, true) << "m\t";
+
+        // (5) humide + fatigue + alcool
+        cout << distanceTotaleHum(vitesseKmh, true, true) << "m\t";
+
+        // Distance en étages
+        cout << "( " << distanceEtage(distanceTotaleHum(vitesseKmh, true, true)) << " etages ! )";
+
+        cout << endl;
+    }
+
+    
+    cout << "(a) reaction" << endl
+         << "(b) freinage" << endl
+         << "(1) normal" << endl
+         << "(2) fatigue" << endl
+         << "(3) alcool" << endl
+         << "(4) fatigue+alcool" << endl
+         << "(5) + route humide" << endl
+         << "autres : freins, pneus ou amortisseurs deficients/masse du vehicule/etc." << endl;
+
     return 0;
 }
